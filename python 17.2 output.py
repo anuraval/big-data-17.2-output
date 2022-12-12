cf9cbc4454b119cc06c33aa746d0678527d52737
anuraval <119819815+anuraval@users.noreply.github.com>
2022-12-11 20:46:27 -0600

    Add files via upload

---
+++ python 17.2 output.py
@@ -0,0 +1,116 @@
+Anushka Raval
+
+>>>
+
+	first	last	
+id			
+1	Paul	Deitel	
+2     Harvey	Deitel	
+3      Abbey	Deitel	
+4	 Dan	Quirk	
+5  Alexander	Wald	
+	isbn		                               title	edition	copyright
+0	0135404673	       Intro to	Python for CS and DS	      1	     2020
+1	0132151006	       Internet	& WWW How to Program	      5	     2012
+2	0134743350		         Java How to Program         11	     2018
+3	0133976890		            C How to Program	      8	     2016
+4	0133406954	    Visual Basic 2012 How to Program	      6      2014
+5	0134601548		    Visual C# How to Program	      6      2017
+6	0136151574		    Visual C++ How toProgram          2      2008
+7	0134448235		          C++ How to Program         10	     2017
+8	0134444302		      Android How to Program	      3	     2017
+9       0134289366	           Android 6 for Programmers	      3	     2016
+        id	  isbn
+0	1   0134289366
+1	2   0134289366
+2	5   0134289366
+3	1   0135404673
+4	2   0135404673
+        first	 last
+0	 Paul    Deitel
+1      Harvey    Deitel
+2	Abbey    Deitel
+3	  Dan	 Quirk
+4   Alexander	 Wald
+                                title   edition  copyright
+0	Intro to Python for CS and DS	1	 2020
+1	          Java How to Program  11        2018
+2	     Visual C# How to Program	6	 2017
+3	           C++ How to Program  10	 2017
+4	       Android How to Program	3	 2017
+       first    last
+id
+1	Paul    Deitel
+2     Harvey    Deitel
+3      Abbey    Deitel
+this
+       first	last
+id
+3	Abbey Deitel 
+                               title
+0	   Android 6 for Programmers
+1	      Android How to Program
+2	            C How to Program
+3	          C++ How to Program
+4      Internet & WWW How to Program
+5      Intro to Python for CS and DS
+6	         Java How to Program
+7   Visual Basic 2012 How to Program
+8	    Visual C# How to Program
+9	   Visual C++ How to Program
+	first	last	
+id		
+3	Abbey   Deitel	
+2      Harvey   Deitel	
+1	 Paul   Deitel	
+4	  Dan    Quirk	
+5   Alexander     Wald	
+	first	last	
+id		
+5   Alexander	  Wald	
+4	  Dan	 Quirk	
+3	Abbey   Deitel	
+2      Harvey   Deitel	
+1	 Paul   Deitel	
+	      isbn		                 title	edition	 copyright
+0	0134444302	        Android	How to Program        3	      2017
+1	0133976890	              C How to Program	      8	      2016
+2	0134448235	             C++ Ho to Program	     10	      2017
+3	0132151006	 Internet & WWW How to Program	      5	      2012
+4	0134743350	           Java How to Program	     11	      2018
+5	0133406954    Visual Basic 2012 How to Program	      6	      2014
+6	0134601548	      Visual C#	How to Program	      6	      2017
+7	0136151574	     Visual C++ How to Program	      2	      2008
+	first	 last	    isbn	
+0	Abbey  Deitel 0132151006	
+1	Abbey  Deitel 0133406954	
+2      Harvey  Deitel 0134289366	
+3      Harvey  Deitel 0135404673	
+4      Harvey  Deitel 0132151006	
+	first	last	
+id		
+1	 Paul   Deitel	
+2      Harvey   Deitel	
+3	Abbey   Deitel	
+4	  Dan    Quirk	
+5   Alexander	  Wald	
+6	  Sue	   Red	
+1		
+	first	last	
+id	
+1	 Paul	Deitel
+2      Harvey	Deitel
+3	Abbey	Deitel
+4	  Dan	 Quirk
+5   Alexander	  Wald
+6	  Sue	 Black
+1		
+	first	last
+id		
+1	 Paul	Deitel
+2      Harvey	Deitel
+3	Abbey	Deitel
+4	  Dan	 Quirk
+5   Alexander	  Wald
+>>>		
+
